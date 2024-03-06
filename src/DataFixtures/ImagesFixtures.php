cc909ec32674b5b3c66a51eebd981bc8c8d5527e
Antoine-NVE <antoinenavette@gmail.com>
2024-03-06 11:27:31 +0100

    Fin épisode 13 : accès aux données - création de la page détails des produits

--- src/DataFixtures/ImagesFixtures.php
+++ src/DataFixtures/ImagesFixtures.php
@@ -22,7 +22,15 @@ class ImagesFixtures extends Fixture implements DependentFixtureInterface
         for ($i = 0; $i < 100; $i++) {
             $image = new Images();
             $image->setName("/tmp/" . $this->slugger->slug($faker->text(25))->lower() . ".png");
-            $product = $this->getReference("prod-" . rand(1, 10));
+            $product = $this->getReference("prod-" . rand(1, 20));
+            $image->setProducts($product);
+            $manager->persist($image);
+        }
+
+        for ($i = 0; $i < 3; $i++) {
+            $image = new Images();
+            $image->setName("ecran0" . $i + 1 . ".jpg");
+            $product = $this->getReference("prod-21");
             $image->setProducts($product);
             $manager->persist($image);
         }
--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -26,13 +26,27 @@ class ProductsFixtures extends Fixture
             $product->setPrice($faker->numberBetween(900, 150000));
             $product->setStock($faker->numberBetween(0, 10));
 
-            $category = $this->getReference("cat-" . rand(1, 8));
+            $category = $this->getReference("cat-" . [2, 3, 4, 6, 7, 8][rand(0, 5)]);
             $product->setCategories($category);
 
             $this->setReference("prod-" . $i + 1, $product);
 
             $manager->persist($product);
         }
+
+        $product = new Products();
+        $product->setName("Joli écran");
+        $product->setDescription("Un très joli écran qui sert à afficher des choses");
+        $product->setSlug($this->slugger->slug($product->getName())->lower());
+        $product->setPrice($faker->numberBetween(900, 150000));
+        $product->setStock($faker->numberBetween(0, 10));
+
+        $category = $this->getReference("cat-3");
+        $product->setCategories($category);
+
+        $this->setReference("prod-21", $product);
+
+        $manager->persist($product);
 
         $manager->flush();
     }
